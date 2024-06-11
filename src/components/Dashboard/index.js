dab9f922cb20224f8e24a6f5ca0ff2c90d415000
Kristian <10221425@ucn.dk>
2024-06-11 10:40:51 +0200

    Get all documents in a collection

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,79 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import Swal from 'sweetalert2';
+import React, { useState, useEffect } from "react"
+import Swal from "sweetalert2"
 
-import Header from './Header';
-import Table from './Table';
-import Add from './Add';
-import Edit from './Edit';
+import Header from "./Header"
+import Table from "./Table"
+import Add from "./Add"
+import Edit from "./Edit"
 
-import { employeesData } from '../../data';
+// import { employeesData } from '../../data';
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "../../config/firestore"
 
 const Dashboard = ({ setIsAuthenticated }) => {
-  const [employees, setEmployees] = useState(employeesData);
-  const [selectedEmployee, setSelectedEmployee] = useState(null);
-  const [isAdding, setIsAdding] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
+  const [employees, setEmployees] = useState()
+  const [selectedEmployee, setSelectedEmployee] = useState(null)
+  const [isAdding, setIsAdding] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+
+  const getEmployees = async () => {
+    const querySnapshot = await getDocs(collection(db, "employees"))
+    const employees = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+
+    setEmployees(employees)
+  }
 
   useEffect(() => {
     // TODO: create getEmployees function and call it here
-  }, []);
+    getEmployees()
+  }, [])
 
   const handleEdit = id => {
-    const [employee] = employees.filter(employee => employee.id === id);
+    const [employee] = employees.filter(employee => employee.id === id)
 
-    setSelectedEmployee(employee);
-    setIsEditing(true);
-  };
+    setSelectedEmployee(employee)
+    setIsEditing(true)
+  }
 
   const handleDelete = id => {
     Swal.fire({
-      icon: 'warning',
-      title: 'Are you sure?',
+      icon: "warning",
+      title: "Are you sure?",
       text: "You won't be able to revert this!",
       showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'No, cancel!',
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
     }).then(result => {
       if (result.value) {
-        const [employee] = employees.filter(employee => employee.id === id);
+        const [employee] = employees.filter(employee => employee.id === id)
 
         // TODO delete document
 
         Swal.fire({
-          icon: 'success',
-          title: 'Deleted!',
+          icon: "success",
+          title: "Deleted!",
           text: `${employee.firstName} ${employee.lastName}'s data has been deleted.`,
           showConfirmButton: false,
           timer: 1500,
-        });
+        })
 
-        const employeesCopy = employees.filter(employee => employee.id !== id);
-        setEmployees(employeesCopy);
+        const employeesCopy = employees.filter(employee => employee.id !== id)
+        setEmployees(employeesCopy)
       }
-    });
-  };
+    })
+  }
 
   return (
     <div className="container">
       {!isAdding && !isEditing && (
         <>
-          <Header
-            setIsAdding={setIsAdding}
-            setIsAuthenticated={setIsAuthenticated}
-          />
-          <Table
-            employees={employees}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
-          />
+          <Header setIsAdding={setIsAdding} setIsAuthenticated={setIsAuthenticated} />
+          <Table employees={employees} handleEdit={handleEdit} handleDelete={handleDelete} />
         </>
       )}
       {isAdding && (
-        <Add
-          employees={employees}
-          setEmployees={setEmployees}
-          setIsAdding={setIsAdding}
-        />
+        <Add employees={employees} setEmployees={setEmployees} setIsAdding={setIsAdding} />
       )}
       {isEditing && (
         <Edit
@@ -84,7 +83,7 @@ const Dashboard = ({ setIsAuthenticated }) => {
         />
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
